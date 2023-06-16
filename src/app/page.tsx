553a9f539f11a28e4c43dcce5822b86dc005f39a
Daniel Haro Contreras <daniel@appfluence.com>
2023-06-16 12:51:55 +0200

    First version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,100 @@
-import Image from 'next/image'
+type WaitTime = number | null;
 
-export default function Home() {
+type WaitingTime = {
+  id: string;
+  time: WaitTime;
+};
+
+type Attraction = {
+  id: number;
+  attributes: {
+    name: string;
+    FTPName: string;
+    openingTime: string;
+    closed: boolean;
+    closedReason: string | null;
+  };
+};
+
+type Attractions = {
+  data: Attraction[];
+  meta: {
+    total: number;
+  };
+};
+
+async function getWaitingTimes() {
+  const res = await fetch(
+    "https://s05y9cqvq5.execute-api.eu-west-1.amazonaws.com/default/getWaitingTimes"
+  );
+  return (await res.json()) as WaitingTime[];
+}
+
+async function getAttractions() {
+  const res = await fetch(
+    "https://api.qa.env.mobile.portaventuraworld.com/api/attractions?locale=en&pagination%5Blimit%5D=10000"
+  );
+  return (await res.json()) as Attractions;
+}
+
+function waitTimeText(time: WaitTime) {
+  if (time === null) {
+    return "?";
+  }
+
+  const hours = Math.floor(time / 60);
+  const minutes = time % 60;
+  const minutesString = `${minutes}`.padStart(2, "0");
+
+  return `${hours}:${minutesString}`;
+}
+
+export default async function Home() {
+  const waitTimesData = getWaitingTimes();
+  const attractionsData = getAttractions();
+
+  const [waitTimes, attractions] = await Promise.all([
+    waitTimesData,
+    attractionsData,
+  ]);
+
+  const waitingTimesPopulated = waitTimes.map((waitTime) => {
+    const attraction = attractions.data.find(
+      (attraction) => attraction.attributes.FTPName === waitTime.id
+    );
+    return {
+      ...waitTime,
+      attraction,
+    };
+  });
+
+  // Sort by name
+  const waitingTimesSorted = waitingTimesPopulated.sort((a, b) => {
+    if (!a.attraction || !b.attraction) {
+      return 0;
+    }
+    if (a.attraction.attributes.name < b.attraction.attributes.name) {
+      return -1;
+    }
+    if (a.attraction.attributes.name > b.attraction.attributes.name) {
+      return 1;
+    }
+    return 0;
+  });
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className="px-5 py-4">
+      <h1 className="text-4xl mb-6">PortAventura Park Waiting Times</h1>
+      <ul>
+        {waitingTimesSorted.map((waitTime) => {
+          return (
+            <li key={waitTime.id}>
+              <strong>{waitTime.attraction?.attributes.name}:</strong>{" "}
+              {waitTime.time === null ? "?" : waitTimeText(waitTime.time)}
+            </li>
+          );
+        })}
+      </ul>
     </main>
-  )
+  );
 }
